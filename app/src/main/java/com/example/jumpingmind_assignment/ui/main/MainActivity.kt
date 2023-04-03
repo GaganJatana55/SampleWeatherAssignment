f4ac875f67f32937a278e9a35d8b06e3b6c7b64d
Gagan <gaganjatana12@gmail.com>
2023-04-03 10:27:11 +0530

    disable night modenot supporting yet

--- app/src/main/java/com/example/jumpingmind_assignment/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/jumpingmind_assignment/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.jumpingmind_assignment.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.jumpingmind_assignment.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main)
 
     }
